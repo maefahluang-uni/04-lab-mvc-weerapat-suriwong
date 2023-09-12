728b61488c1718836c443da6df66df88d3ba3346
Weerapat suriwong <6531503123@lamduan.mfu.ac.th>
2023-09-12 16:41:31 +0700

    lab04

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -5,15 +5,51 @@ import java.util.Date;
 public class Concert {
 
     //TODO: add attributes
-
+private int id;
+private String title;
+private String performer;
+private Date date;
+private String description ;
     public Concert() {
     }
     public Concert(String title, String description) {
         //TODO: set attributes
-
+this.title=title;
+this. description= description ;
     }
- 
-    //TODO: add getters and setters for all attributes
-    // You can use Source action/generate getter setter function
+ public String getTitle() {
+    // TODO: return title
+    return title;
+ }
+ public String getDescription(){
+    //TODO: return description
+    return description ;
+}
+//Todo: add getters and setters
+public int getld (){
+    return id;
+}
+public void setld(int id){
+    this.id = id ;
+}
+ public void setTitle (String title)
+{
+    this.title = title;
+}
+
+public String getPerformer(){
+return performer;
+}
+public void setPerformer(String performer){
+    this.performer = performer;
 
 }
+public void setDate(Date date){
+    this.date = date;
+}
+public void setDescription(String description){
+    this.description = description;
+  }
+}
+ //TODO: add getters and setters for all attributes
+    // You can use Source action/generate getter setter function
--- src/main/java/th/mfu/ConcertController.java
+++ src/main/java/th/mfu/ConcertController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Locale;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.objenesis.instantiator.gcj.GCJInstantiator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
@@ -20,44 +21,59 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ConcertController {
     // TODO: create hashmap of concerts for storing data
-
+private int nextld = 1;
+private HashMap<Integer,Concert>concertMap = new HashMap<Integer,Concert>();
     //TODO: add initbinder to convert date
+    @InitBinder
+public final void initbinderUsuariosFormValidator(final WebDataBinder binder, final Locale locale) {
+ final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+ binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+}
 
-    @GetMapping("/concerts")
+   @GetMapping("/concerts")
     public String listConcerts(Model model) {
         // TODO: add concerts to model
+    model.addAttribute("concerts",concertMap.values());
         // TODO: return a template to list concerts
-        return "";
+        return "list-concert";
     }
 
     @GetMapping("/add-concert")
     public String addAConcertForm(Model model) {
         // TODO: pass blank concert to a form
+     model.addAttribute("concert", new Concert());
         // TODO: return a template for concert form
-        return "";
+        return "add-concert-form";
     }
 
     @PostMapping("/concerts")
     public String saveConcert(@ModelAttribute Concert concert) {
         // TODO: add concert to list of concerts
+     concert.setld(nextld);
         // TODO: increment nextId
+      concertMap.put(nextld,concert);
+     nextld++;
+
         // TODO: redirect to list concerts
-        return "";
+        return "redirect:/concerts";
     }
 
     @GetMapping("/delete-concert/{id}")
     public String deleteConcert(@PathVariable int id) {
         // TODO: remove concert from list of concerts
+        concertMap.remove(id);
         // TODO: redirect to list concerts
-        return "";
+        return "redirect:/concerts";
     }
 
     
     @GetMapping("/delete-concert")
     public String removeAllConcerts() {
         //TODO: clear all employees and reset id
+        concertMap.clear();
+        nextld = 1;
         // TODO: redirect to list concerts
-        return "";
+        return "redirect:/concerts";
     }
 
 }
